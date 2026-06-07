2cd73eaa98c5464332cc7e435d97fcb22986219b
miumau116 <raheelnoorm@gmail.com>
2026-06-07 10:38:43 +0300

    Create coupon.js

---
+++ coupon.js
@@ -0,0 +1,7 @@
+setTimeout(() => {
+    document.getElementById("couponPopup").style.display = "block";
+}, 5000);
+
+function closePopup(){
+    document.getElementById("couponPopup").style.display = "none";
+}
